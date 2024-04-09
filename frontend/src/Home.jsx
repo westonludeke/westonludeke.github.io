c0afed9527bd952fc6140accf6d8ce9db554aff3
westonludeke <westonludeke@gmail.com>
2024-04-09 16:42:41 -0500

    rename components to JSX; update Home.jsx text; add home class styling to App.css

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -4,8 +4,7 @@ import './App.css';
 function Home() {
   return (
     <div className="home">
-      <h1>Hello World! I'm Weston.</h1>
-      <h1>A Houston - based Full Stack Software Engineer.</h1>
+      <h1>Hello World! I'm Weston Full Stack Software Engineer</h1>
         <p>Recently I built Campfire, an open-source, self-hosted deploy preview solution for containerized, headless frontends</p>
     </div>
   );
