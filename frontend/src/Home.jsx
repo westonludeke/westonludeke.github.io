6858fc585f4453437eb187908c443c528e48170f
westonludeke <westonludeke@gmail.com>
2024-04-11 16:43:02 -0500

    updated Home.jsx to use sample project logo; updated CSS to style image; added case study and github test links

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 // import Navbar from './Navbar';
 import './App.css';
+import campfirePng from './campfire.png';
 
 function Home() {
   return (
@@ -8,6 +9,15 @@ function Home() {
       <div className="content">
         <h1>Howdy! I'm Weston, a Software Engineer from Houston.</h1>
         <p>I recently developed Campfire, an open-source, self-hosted deploy preview solution for containerized, headless frontends.</p>
+      </div>
+      <div className="logo-and-links">
+        <div className="logo-section">
+          <img src={campfirePng} alt="campfire png"/>
+        </div>
+        <div className="campfire-links">
+          <p>Case Study: <a href="https://campfirepreviews.github.io">https://campfirepreviews.github.io</a></p>
+          <p>GitHub: <a href="https://github.com/campfire-previews/campfire-app">https://github.com/campfire-previews/campfire-app</a></p>
+        </div>
       </div>
     </div>
   );
