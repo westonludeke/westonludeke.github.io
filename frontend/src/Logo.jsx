1fefcf71966d59dd478066b3eccd77bd84859641
westonludeke <westonludeke@gmail.com>
2024-04-15 20:30:51 -0500

    fork HTML5 UP template

--- frontend/src/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Logo() {
-  return (
-    <img src="https://raw.githubusercontent.com/campfire-previews/campfire-app/d112069d0b9b2117d1ab8ef26fb7c21220c82901/src/assets/campfire.svg" alt="Logo" className="campfire-logo" />
-  );
-}
-
-export default Logo;
